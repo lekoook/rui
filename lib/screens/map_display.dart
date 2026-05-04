89d2d1183fc7f719161081d4fb0de23c1c29a624
lekoook <kokteng1313@gmail.com>
2026-05-05 01:50:24 +0800

    Test draw image

--- lib/screens/map_display.dart
+++ lib/screens/map_display.dart
@@ -1,5 +1,46 @@
+
+import 'package:flutter/services.dart';
+
 import '../data/robot_status_model.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/widget_previews.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
+
+@Preview(name: 'Map Display')
+Widget mapDisplay() {
+  RobotStatusViewModel vm = RobotStatusViewModel();
+  return MapDisplay(robotStatusViewModel: vm);
+}
+
+class MapPainter extends CustomPainter {
+  MapPainter({
+    required this.mapImage
+  });
+
+  final ui.Image? mapImage;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (mapImage == null) {
+      return;
+    }
+    canvas.save();
+
+    // canvas.scale(1, -1);
+    canvas.drawImage(
+      mapImage!,
+      Offset((size.width - mapImage!.width) / 2.0, (size.height - mapImage!.height) / 2.0),
+      Paint()
+    );
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
 
 class MapDisplay extends StatefulWidget {
   const MapDisplay({required this.robotStatusViewModel, super.key});
@@ -15,10 +56,30 @@ class _MapDisplayState extends State<MapDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.gray,
-      child: Text('Map goes here'),
+    return FutureBuilder<ui.Image>(
+      future: loadImage('test_map.png'),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return CustomPaint(
+            painter: MapPainter(mapImage: snapshot.data),
+          );
+          // return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Container();
+        }
+      }
     );
+  }
+
+  Future<ui.Image> loadImage(String asset) async {
+    final data = await rootBundle.load(asset);
+    final bytes = data.buffer.asUint8List();
+
+    final codec = await ui.instantiateImageCodec(bytes);
+    final frame = await codec.getNextFrame();
+
+    return frame.image;
   }
 }
